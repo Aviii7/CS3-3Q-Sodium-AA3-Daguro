80db3a6f247a8329b81c69909db578f373adb3ba
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-03-03 14:44:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunc() {
   let a = prompt("Input the message to be verified: ");
-  if ((a.indexOf(FR) || (a.indexOf(FR) && a.indexOf(AI))) >= 0) {
+  if ((a.indexOf("FR") || (a.indexOf("FR") && a.indexOf("AI"))) >= 0) {
     document.getElementById("result").innerHTML = 'The message "' + a + '" is legitimate!';
-  }else if ((a.indexOf(AI) || a.indexOf(Ai) || a.indexOf(aI)) >= 0)  {
+  }else if ((a.indexOf("AI") || a.indexOf("Ai") || a.indexOf("aI")) >= 0)  {
     document.getElementById("result").innerHTML = 'The message "' + a + '" is fake!';
   }else{
     document.getElementById("result").innerHTML = 'The message "' + a + '" is not yet encoded!';
